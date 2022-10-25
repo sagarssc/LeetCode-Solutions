dedff44e757c8ead1ce5d6e0bd5f3408f5be90be
sagarssc <sagarsinghchauhan49@gmail.com>
2022-10-25 12:33:49 +0530

    Time: 114 ms (89.26%), Space: 14.6 MB (82.92%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        need, missing = collections.Counter(t), len(t)
+        i = I = J = 0
+        for j, c in enumerate(s, 1):
+            missing -= need[c] > 0
+            need[c] -= 1
+            # print(need)
+            if not missing:
+                # print('IJ. ',I,'   ',J)
+                # print('ij. ',i,'   ',j)
+                # print(need)
+                while i < j and need[s[i]] < 0:
+                    need[s[i]] += 1
+                    i += 1
+                if not J or j - i <= J - I:
+                    I, J = i, j
+        return s[I:J]
