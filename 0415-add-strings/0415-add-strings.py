94bf6192e9901bfd46bad26c102888a05b722e84
sagarssc <sagarsinghchauhan49@gmail.com>
2022-10-25 13:56:17 +0530

    Time: 100 ms (21.17%), Space: 14.5 MB (7.00%) - LeetHub

--- 0415-add-strings/0415-add-strings.py
+++ 0415-add-strings/0415-add-strings.py
@@ -1,3 +1,25 @@
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
-        return str(int(num1)+int(num2))
+        num1 = list(num1)[::-1]
+        num2 = list(num2)[::-1]
+        rem = 0
+        op = []
+        while num1 or num2:
+            if num1:
+                x = int(num1.pop(0))
+            else:
+                x = 0
+            if num2:
+                y = int(num2.pop(0))
+            else:
+                y = 0
+            
+            p = rem + x + y
+            # print(num1,'  ',num2,'   ',p)
+            rem = p // 10
+            v = p % 10
+            op.append(str(v))
+        if rem:
+            op.append(str(rem))
+        return ''.join(op[::-1])
+        
