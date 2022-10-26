72f1c552f6b0ad6dd4563c9afe83a79b9a69361e
sagarssc <sagarsinghchauhan49@gmail.com>
2022-10-26 11:54:15 +0530

    Time: 2618 ms (21.30%), Space: 33.2 MB (73.31%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -4,9 +4,6 @@ class Solution:
         d = {0:-1}
         for i in range(len(nums)):
             mod = (mod + nums[i]) % k
-            # # print(mod)
-            # if mod == 0:
-            #     return True
             if mod not in d:
                 d[mod] = i
             else:
