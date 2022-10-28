b1ea80254fc109bf8fae1cce6598cc3cc406d6a9
sagarssc <sagarsinghchauhan49@gmail.com>
2022-10-28 11:12:59 +0530

    Time: 85 ms (99.92%), Space: 17 MB (96.53%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,10 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        d = {}
+        for s in strs:
+            sorted_s = ''.join(sorted(s))
+            if sorted_s in d:
+                d[sorted_s].append(s)
+            else:
+                d[sorted_s] = [s]
+        return list(d.values())
